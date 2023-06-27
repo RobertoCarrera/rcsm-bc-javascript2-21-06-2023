cc339a3c4dcfdf83e224fd93b68a0a5c977cb709
RobertoCarrera <robertocarrera93@gmail.com>
2023-06-27 12:45:47 +0200

    La he liado?

---
+++ js/Ejercicio8.js
@@ -0,0 +1,8 @@
+// 8. Dados los arrays javascript1 = ["DOM", "BOM", "Funciones", "Arrays"] 
+// y javascript2 = ["Objetos", "Arrays", "ParseInt"], 
+// crea una función que determine si se repite algún elemento y lo saque por consola.
+
+javascript1 = ["DOM", "BOM", "Funciones", "Arrays"];
+javascript2 = ["Objetos", "Arrays", "ParseInt"];
+
+
